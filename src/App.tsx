5fa3bbe2dfad72e682165acb30384a2c5088cca4
alonzo245 <alon.alush.mail@gmail.com>
2022-10-11 20:50:15 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { FC } from 'react';
-import { GlobalStateProvider, useGlobalState } from './context/useGlobalState';
-import { ThemeStateProvider } from './context/useThemeState';
-import IndexRouter from './routes';
+import { FC } from "react";
+import { GlobalStateProvider, useGlobalState } from "./context/useGlobalState";
+import { ThemeStateProvider } from "./context/useThemeState";
+import IndexRouter from "./routes";
 
 const App: FC = () => {
-    const {} = useGlobalState();
-    return (
-        <ThemeStateProvider>
-            <GlobalStateProvider>
-                <IndexRouter />
-            </GlobalStateProvider>
-        </ThemeStateProvider>
-    );
+  const {} = useGlobalState();
+  return (
+    <ThemeStateProvider>
+      <GlobalStateProvider>
+        <IndexRouter />
+      </GlobalStateProvider>
+    </ThemeStateProvider>
+  );
 };
 
 export default App;
