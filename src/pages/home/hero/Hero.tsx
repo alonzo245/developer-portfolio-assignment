32a9979e838a5912ef6cdd4db8b8336d42b5a221
alonzo245 <alon.alush.mail@gmail.com>
2022-10-11 21:26:15 +0300

    save

--- src/pages/home/hero/Hero.tsx
+++ src/pages/home/hero/Hero.tsx
@@ -1,151 +1,151 @@
-import styled from '@emotion/styled';
-import { useThemeState } from '../../../context/useThemeState';
-import { THEMES, ThemeType } from '../../../theme';
-import Colors from '../../../theme/Colors';
-import { DESKTOP_MQ } from '../../../theme/theme.constants';
-import { githubIcon, linkedinIcon } from './icons';
-import { IoDocumentAttachOutline } from 'react-icons/io5';
-import ReactTooltip from 'react-tooltip';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import styled from "@emotion/styled";
+import { useThemeState } from "../../../context/useThemeState";
+import { THEMES, ThemeType } from "../../../theme";
+import Colors from "../../../theme/Colors";
+import { DESKTOP_MQ } from "../../../theme/theme.constants";
+import { githubIcon, linkedinIcon } from "./icons";
+import { IoDocumentAttachOutline } from "react-icons/io5";
+import ReactTooltip from "react-tooltip";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
-export const GITHUB_LINK = 'https://github.com/alonzo245';
-export const LINKEDIN_LINK = 'https://www.linkedin.com/in/alonalush';
+export const GITHUB_LINK = "https://github.com/alonzo245";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/alonalush";
 export const CV_LINK =
-    'https://firebasestorage.googleapis.com/v0/b/portfolio-tutorial-d06e5.appspot.com/o/example_doc.pdf?alt=media&token=6f6b470a-2d46-48e8-9691-9616a5274665';
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-tutorial-d06e5.appspot.com/o/example_doc.pdf?alt=media&token=6f6b470a-2d46-48e8-9691-9616a5274665";
 
 const Hero: React.FC = () => {
-    const { theme } = useThemeState();
+  const { theme } = useThemeState();
 
-    return (
-        <Container theme={theme}>
-            <Row>
-                <H1 theme={theme}>Portfolio Website</H1>
-                <P>Level Up your Skills</P>
-                <StyledAnchorLink href="#about" theme={theme}>
-                    About me
-                </StyledAnchorLink>
-            </Row>
-            <RowContact>
-                <Link href={CV_LINK} data-tip data-for="cv">
-                    <StyledIoDocumentAttachOutline color={Colors.white} size={50} />
-                </Link>
-                <Link href={GITHUB_LINK} target="_blank" data-tip data-for="github">
-                    <span> {githubIcon(50)}</span>
-                </Link>
-                <Link href={LINKEDIN_LINK} target="_blank" data-tip data-for="linkedin">
-                    <span>{linkedinIcon(50)}</span>
-                </Link>
-                <ReactTooltip id="cv" place="bottom" effect="solid">
-                    My CV
-                </ReactTooltip>
-                <ReactTooltip id="linkedin" place="bottom" effect="solid">
-                    Linkedin Profile
-                </ReactTooltip>
-                <ReactTooltip id="github" place="bottom" effect="solid">
-                    GitHub Profile
-                </ReactTooltip>
-            </RowContact>
-        </Container>
-    );
+  return (
+    <Container theme={theme}>
+      <Row>
+        <H1 theme={theme}>Portfolio Website</H1>
+        <P>Level Up your Skills :)</P>
+        <StyledAnchorLink href="#about" theme={theme}>
+          About me
+        </StyledAnchorLink>
+      </Row>
+      <RowContact>
+        <Link href={CV_LINK} data-tip data-for="cv">
+          <StyledIoDocumentAttachOutline color={Colors.white} size={50} />
+        </Link>
+        <Link href={GITHUB_LINK} target="_blank" data-tip data-for="github">
+          <span> {githubIcon(50)}</span>
+        </Link>
+        <Link href={LINKEDIN_LINK} target="_blank" data-tip data-for="linkedin">
+          <span>{linkedinIcon(50)}</span>
+        </Link>
+        <ReactTooltip id="cv" place="bottom" effect="solid">
+          My CV
+        </ReactTooltip>
+        <ReactTooltip id="linkedin" place="bottom" effect="solid">
+          Linkedin Profile
+        </ReactTooltip>
+        <ReactTooltip id="github" place="bottom" effect="solid">
+          GitHub Profile
+        </ReactTooltip>
+      </RowContact>
+    </Container>
+  );
 };
 
 export default Hero;
 
 const Container = styled.div`
-    display: flex;
-    height: 100%;
-    justify-content: flex-start;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  height: 100%;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
 
-    ${DESKTOP_MQ} {
-        justify-content: space-around;
-        flex-direction: row;
-        align-items: center;
-    }
+  ${DESKTOP_MQ} {
+    justify-content: space-around;
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 export const StyledIoDocumentAttachOutline = styled(IoDocumentAttachOutline)`
-    cursor: pointer;
+  cursor: pointer;
 `;
 
 const H1 = styled.h1<{ theme: ThemeType }>`
-    font-weight: bolder;
-    font-size: 70px;
-    margin-top: 40px;
-    color: ${(p) => THEMES[p.theme.themeName]?.h1};
-    text-align: center;
-    position: relative;
+  font-weight: bolder;
+  font-size: 70px;
+  margin-top: 40px;
+  color: ${(p) => THEMES[p.theme.themeName]?.h1};
+  text-align: center;
+  position: relative;
 
-    ${DESKTOP_MQ} {
-        margin: 0;
-        margin-top: 80px;
-        font-size: 90px;
-        text-align: left;
-        margin-top: 0;
-    }
+  ${DESKTOP_MQ} {
+    margin: 0;
+    margin-top: 80px;
+    font-size: 90px;
+    text-align: left;
+    margin-top: 0;
+  }
 
-    &::after {
-        content: 'Portfolio Website';
-        color: ${Colors.white};
-        z-index: 1;
-        position: absolute;
-        bottom: 8px;
-        left: 0;
-        right: 0;
-    }
+  &::after {
+    content: "Portfolio Website";
+    color: ${Colors.white};
+    z-index: 1;
+    position: absolute;
+    bottom: 8px;
+    left: 0;
+    right: 0;
+  }
 `;
 
 const P = styled.p`
-    font-size: 30px;
-    font-weight: 300;
-    margin-bottom: 30px;
-    font-weight: bold;
+  font-size: 30px;
+  font-weight: 300;
+  margin-bottom: 30px;
+  font-weight: bold;
 
-    ${DESKTOP_MQ} {
-        font-size: 40px;
-    }
+  ${DESKTOP_MQ} {
+    font-size: 40px;
+  }
 `;
 
 const Row = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-left: 0px;
-    margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0px;
+  margin-bottom: 30px;
 
-    ${DESKTOP_MQ} {
-        display: flex;
-        justify-content: flex-start;
-        align-items: flex-start;
-        margin-left: 80px;
-        margin-bottom: 200px;
-    }
+  ${DESKTOP_MQ} {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    margin-left: 80px;
+    margin-bottom: 200px;
+  }
 `;
 
 const StyledAnchorLink = styled(AnchorLink)<{ theme: ThemeType }>`
-    width: 200px;
-    height: 55px;
-    border-radius: 20px;
-    background: ${(p) => THEMES[p.theme.themeName]?.heroButton};
-    text-align: center;
-    font-size: 30px;
-    font-weight: 300;
-    padding: 5px 20px;
-    color: ${Colors.white};
+  width: 200px;
+  height: 55px;
+  border-radius: 20px;
+  background: ${(p) => THEMES[p.theme.themeName]?.heroButton};
+  text-align: center;
+  font-size: 30px;
+  font-weight: 300;
+  padding: 5px 20px;
+  color: ${Colors.white};
 `;
 
 const RowContact = styled.div`
-    display: flex;
-    padding-bottom: 200px;
+  display: flex;
+  padding-bottom: 200px;
 
-    ${DESKTOP_MQ} {
-        justify-content: space-around;
-    }
+  ${DESKTOP_MQ} {
+    justify-content: space-around;
+  }
 `;
 
 const Link = styled.a`
-    display: block;
-    margin: 20px;
+  display: block;
+  margin: 20px;
 `;
